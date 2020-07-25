27d92c8adc39ebb14aad6d63f5222dfefb20e50a
viktoriazaharova <zaharova181114@gmail.com>
2020-07-25 16:15:14 +0300

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,7 @@
+$('.main-home__slider').slick({
+    slidesToShow: 1,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    fade: true
+});
