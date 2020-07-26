6244db7c3965353ac4a37b7d191cd5a692d3303e
viktoriazaharova <zaharova181114@gmail.com>
2020-07-26 20:27:33 +0300

    desktop

--- app/js/common.js
+++ app/js/common.js
@@ -5,3 +5,18 @@ $('.main-home__slider').slick({
     nextArrow: '<button type="button" class="slick-next"></button>',
     fade: true
 });
+
+$('.down').click(function () {
+    var $input = $(this).parent().find('input');
+    var count = parseInt($input.val()) - 1;
+    count = count < 1 ? 1 : count;
+    $input.val(count);
+    $input.change();
+    return false;
+});
+$('.up').click(function () {
+    var $input = $(this).parent().find('input');
+    $input.val(parseInt($input.val()) + 1);
+    $input.change();
+    return false;
+});
